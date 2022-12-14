7c4a030497f6593a9775d15ad0a367ca3f6d2b47
dipeshsingh253 <sinhdipesh@gmail.com>
2022-12-14 15:44:47 +0530

    initiating spring security implementation project

---
+++ SpringSecurityImplementation/src/main/java/com/customsecurity/SpringSecurityImplementation/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.customsecurity.SpringSecurityImplementation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String helloGeneral() {
+        return "Hello World...To Everyone!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World...!";
+    }
+
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello World User...!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello World Admin...!";
+    }
+}
---
+++ SpringSecurityImplementation/src/test/java/com/customsecurity/SpringSecurityImplementation/SpringSecurityImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customsecurity.SpringSecurityImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
