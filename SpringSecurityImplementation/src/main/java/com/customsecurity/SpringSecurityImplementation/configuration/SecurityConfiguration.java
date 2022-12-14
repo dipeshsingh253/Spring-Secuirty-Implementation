7f11dad1dd17120fe89e27a42c39ffbeae4de86b
dipeshsingh253 <sinhdipesh@gmail.com>
2022-12-14 16:16:53 +0530

    implementing role based security with spring security

--- SpringSecurityImplementation/src/main/java/com/customsecurity/SpringSecurityImplementation/configuration/SecurityConfiguration.java
+++ SpringSecurityImplementation/src/main/java/com/customsecurity/SpringSecurityImplementation/configuration/SecurityConfiguration.java
@@ -28,7 +28,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/").permitAll()
+                .antMatchers("/hello").authenticated()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
                 .and()
                 .httpBasic();
 
