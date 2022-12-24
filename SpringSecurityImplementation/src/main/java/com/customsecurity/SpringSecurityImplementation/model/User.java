2b183ce89d2f298c1f414397c5fdccf933ed594d
dipeshsingh253 <sinhdipesh@gmail.com>
2022-12-24 08:19:49 +0530

    implementing security not successfull

---
+++ SpringSecurityImplementation/src/main/java/com/customsecurity/SpringSecurityImplementation/model/User.java
@@ -0,0 +1,25 @@
+package com.customsecurity.SpringSecurityImplementation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private String email;
+
+}
---
+++ SpringSecurityImplementation/src/main/java/com/customsecurity/SpringSecurityImplementation/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.customsecurity.SpringSecurityImplementation.repository;
+
+import com.customsecurity.SpringSecurityImplementation.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
